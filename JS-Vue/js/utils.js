0b449ac54488a56c4da2022c1596586e7336bebb
zishu <129256950+Crazyzishu@users.noreply.github.com>
2025-03-06 15:43:18 +0800

    main

--- JS-Vue/js/utils.js
+++ JS-Vue/js/utils.js
@@ -1,3 +1,8 @@
-export function printLog(msg){
-  console.log(msg);
+/**
+ * 打印日志信息到控制台
+ * @param {any} msg - 要打印的日志信息
+ */
+export function printLog(msg) {
+    // 打印日志信息
+    console.log(msg);
 }
